2fc568d6c31c2119698eb78c36512be7407ce132
mdyasenrafe <mdyasenrafe@gmail.com>
2026-02-10 03:12:15 +0600

    Refactor: Simplify AppNavigation by removing onboarding and loading logic, extract screen dimensions utility, and configure React Native assets and Babel worklets.

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,53 +1,10 @@
 import React from 'react';
-import { Audience, getScreenDimensions, MedicineBootstrap } from '../utils';
-import { useOnboarding } from '../context/OnboardingContext';
-import { useAppSelector } from '../redux';
 import { MainNavigation } from './MainNavigation';
-import { OnboardingNavigation } from './OnboardingNavigation';
-
-import { getLoadingImageByAudience } from '../utils/loadingAssets';
-import { FullScreenLoadingImage } from '../components/atom';
+import { getScreenDimensions } from '../utils/screenDimensions';
 
 export function AppNavigation() {
-  const { screenHeight, screenWidth } = getScreenDimensions();
-  const { isHydrated, shouldShowOnboarding, audience } = useOnboarding();
-
-  const hasMedicines = useAppSelector(
-    s => (s.medicine?.items?.length ?? 0) > 0,
-  );
-
-  const loadingSource = getLoadingImageByAudience(audience as Audience);
-
-  if (!isHydrated) {
-    return (
-      <FullScreenLoadingImage
-        source={loadingSource}
-        width={screenWidth}
-        height={screenHeight}
-      />
-    );
-  }
-
-  if (shouldShowOnboarding) {
-    return <OnboardingNavigation />;
-  }
-
-  if (!hasMedicines) {
-    return (
-      <>
-        <MedicineBootstrap />
-        <FullScreenLoadingImage
-          source={loadingSource}
-          width={screenWidth}
-          height={screenHeight}
-        />
-      </>
-    );
-  }
-
   return (
     <>
-      <MedicineBootstrap />
       <MainNavigation />
     </>
   );
